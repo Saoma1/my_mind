5e11e8ae02b69daced367be63d132fbfd3d8ae99
Saoma1 <sotirios.dimitriou@gmail.com>
2024-03-03 11:37:58 +0100

    feat: make greek default, finsih nav, add lang higlight

--- src/_components/shared/navbar.rb
+++ src/_components/shared/navbar.rb
@@ -1,5 +1,5 @@
 class Shared::Navbar < Bridgetown::Component
-  def initialize(metadata:, resource:)
-    @metadata, @resource = metadata, resource
+  def initialize(metadata:, resource:, current_locale:)
+    @metadata, @resource, @current_locale = metadata, resource, current_locale
   end
 end
